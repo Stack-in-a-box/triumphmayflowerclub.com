0138c63cd7fbc005600ce7254cfff9907f0831cd
Andy Davies <andydavies92@outlook.com>
2018-07-22 14:27:22 +0100

    Remove Brian Redshaw's car from the slideshow

--- slideshow/slideshow.js
+++ slideshow/slideshow.js
@@ -8,14 +8,13 @@ $(document).ready(() => {
         { registration: "XAS 929", caption: "John and Barbara Albion’s Mayflower" },
         { registration: "HBX 957", caption: "Mark Smith’s Mayflower" },
         { registration: "DM-42-72", caption: "Nico ten Wolde’s Mayflower" },
-        { registration: "MSK 624", caption: "Brian Redshaw’s Mayflower" },
         { registration: "KSF 804", caption: "Louis Lemaire’s Mayflower" }
     ];
 
     const millisecondsSinceEpoch = new Date();
     const millisecondsPerDay = 8.64e7;
     const daysSinceEpoch = Math.floor(millisecondsSinceEpoch / millisecondsPerDay);
-    const totalNumberOfPhotos = 10;
+    const totalNumberOfPhotos = imageDetails.length;
     const cycleIndex = (daysSinceEpoch % totalNumberOfPhotos);
     const dayInCycle = cycleIndex + 1;
 
