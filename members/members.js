70b68113b63f8e754cd20c73a81effaa3bbb1048
Andi Emma Davies <andidavies92@outlook.com>
2019-07-28 18:27:45 +0100

    Add proof-of-concept code to encrypt/decrypt HTML

--- members/members.js
+++ members/members.js
@@ -1,5 +1,12 @@
 const isAuthenticated = true;
 const membersOnlyRegion = document.getElementById("membersOnly");
 
-if (isAuthenticated) membersOnlyRegion.innerHTML = `<p style="color: green;">Hello, members-only world!</p>`;
+const encryptionPassphrase = "passphrase";
+const stringToBeEncrypted = `<p style="color: green;">Hello, encrypted world!</p>`;
+const contentToBeEncrypted = CryptoJS.enc.Utf8.parse(stringToBeEncrypted);
+const encryptedContent = CryptoJS.AES.encrypt(contentToBeEncrypted, encryptionPassphrase);
+const decryptedContent = CryptoJS.AES.decrypt(encryptedContent, encryptionPassphrase);
+const decryptedString = CryptoJS.enc.Utf8.stringify(decryptedContent);
+
+if (isAuthenticated) membersOnlyRegion.innerHTML = decryptedString;
 else membersOnlyRegion.innerHTML = `<p style="color: red;">You must be logged-in to see this.</p>`;
