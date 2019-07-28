6a6d74eca0ac6fdf59a05f3e909893ea43323921
Andi Emma Davies <andidavies92@outlook.com>
2019-07-28 17:41:16 +0100

    Add conditional rendering depending on auth status

--- members/members.js
+++ members/members.js
@@ -1,3 +1,5 @@
-document.getElementById("membersOnly").innerHTML = `
-    <p>Hello, members-only world!</p>
-`;
+const isAuthenticated = true;
+const membersOnlyRegion = document.getElementById("membersOnly");
+
+if (isAuthenticated) membersOnlyRegion.innerHTML = `<p style="color: green;">Hello, members-only world!</p>`;
+else membersOnlyRegion.innerHTML = `<p style="color: red;">You must be logged-in to see this.</p>`;
