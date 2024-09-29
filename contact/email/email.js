3ac5cb5861f039d912828b0824889fcc6f017c29
Andi Emma Davies-Wilcox <andi@stackinabox.co.uk>
2024-09-29 19:44:29 +0100

    WIP: Email form
    To do:
    - [ ] Search for "mailto" and "@" to look for places where email
          addresses need redacting and replacing with links to the contact
          form.
    - [ ] Add a link/button to the contact page that opens the email
          authoring page with "general enquiry" selected.
    - [ ] Update all email addresses on the contact page to link to the
          email authoring page with the appropriate "purpose" pre-selected.
    - [ ] Update Formspree "project" to limit domain email requests can come
          from to be the prod env only.
    - [ ] Update all forms to the correct emails when everyone has verified.
    - [ ] Deploy and final tests.
    - [ ] Send communication to committee members to explain how to get
          started with the system.

---
+++ contact/email/email.js
@@ -0,0 +1,33 @@
+$(document).ready(() => {
+    const queryStringParameters = new URLSearchParams(window.location.search);
+    const formIdForPurpose = {
+        general: "xblrvaww",
+        chair: "mgvwglzd",
+        vicechair: "mvgpebqj",
+        secretary: "mwpegnqg",
+        treasurer: "mrbznaaz",
+        membership: "xjkbneeg",
+        rallies: "mdknqlle",
+        magazine: "manwrddo",
+        spares: "xqazrddb",
+        regalia: "xldrqwwl",
+        website: "xyzgrddd"
+    };
+
+    const setFormIdOnFormElement = formId => $("#form").attr("action", `https://formspree.io/f/${formId}`);
+
+    if (queryStringParameters.has("purpose")) {
+        const purpose = queryStringParameters.get("purpose");
+
+        if (formIdForPurpose.hasOwnProperty(purpose)) {
+            const formId = formIdForPurpose[purpose];
+            $("#purpose").val(purpose);
+            setFormIdOnFormElement(formId);
+        }
+    }
+
+    $("#purpose").on("change", ({ target }) => {
+        const formId = formIdForPurpose[target.value];
+        $("#form").attr("action", `https://formspree.io/f/${formId}`);
+    });
+});
